22315f91b5d1c6d8f13ed126d15f7479bb1d4ace
RomeoDespres <despres.romeo@gmail.com>
2020-01-31 10:49:25 +0100

    Add setup.py and reshape as a package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pkce',
+    version='1.0',
+    author='Roméo Després',
+    author_email='despres.romeo@gmail.com',
+    description='PKCE Pyhton generator.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RomeoDespres/pkce',
+    packages=['pkce'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
