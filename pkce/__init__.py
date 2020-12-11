05c9d9bea3af14baa2da2576ef6fcd6dfc68cc3b
Juan Pablo Molina Matute <jpmolinamatute@gmail.com>
2020-12-11 16:07:16 -0400

    Implemented simple typing for all function

--- pkce/__init__.py
+++ pkce/__init__.py
@@ -15,7 +15,7 @@ import hashlib
 import base64
 
 
-def generate_code_verifier(length=128):
+def generate_code_verifier(length: int = 128) -> str:
     """Return a random PKCE-compliant code verifier.
 
     Parameters
@@ -34,13 +34,13 @@ def generate_code_verifier(length=128):
         When `43 <= length <= 128` is not verified.
     """
     if not 43 <= length <= 128:
-        msg = 'Parameter `length` must verify `43 <= length <= 128`.'
+        msg = "Parameter `length` must verify `43 <= length <= 128`."
         raise ValueError(msg)
     code_verifier = secrets.token_urlsafe(96)[:length]
     return code_verifier
 
 
-def generate_pkce_pair(code_verifier_length=128):
+def generate_pkce_pair(code_verifier_length: int = 128) -> tuple:
     """Return random PKCE-compliant code verifier and code challenge.
 
     Parameters
@@ -60,17 +60,15 @@ def generate_pkce_pair(code_verifier_length=128):
         When `43 <= code_verifier_length <= 128` is not verified.
     """
     if not 43 <= code_verifier_length <= 128:
-        msg = (
-            'Parameter `code_verifier_length` must verify '
-            '`43 <= code_verifier_length <= 128`.'
-        )
+        msg = "Parameter `code_verifier_length` must verify "
+        msg += "`43 <= code_verifier_length <= 128`."
         raise ValueError(msg)
     code_verifier = generate_code_verifier(code_verifier_length)
     code_challenge = get_code_challenge(code_verifier)
     return code_verifier, code_challenge
 
 
-def get_code_challenge(code_verifier):
+def get_code_challenge(code_verifier: str) -> str:
     """Return the PKCE-compliant code challenge for a given verifier.
 
     Parameters
@@ -89,12 +87,10 @@ def get_code_challenge(code_verifier):
         When `43 <= len(code_verifier) <= 128` is not verified.
     """
     if not 43 <= len(code_verifier) <= 128:
-        msg = (
-            'Parameter `code_verifier` must verify '
-            '`43 <= len(code_verifier) <= 128`.'
-        )
+        msg = "Parameter `code_verifier` must verify "
+        msg += "`43 <= len(code_verifier) <= 128`."
         raise ValueError(msg)
-    hashed = hashlib.sha256(code_verifier.encode('ascii')).digest()
+    hashed = hashlib.sha256(code_verifier.encode("ascii")).digest()
     encoded = base64.urlsafe_b64encode(hashed)
-    code_challenge = encoded.decode('ascii')[:-1]
+    code_challenge = encoded.decode("ascii")[:-1]
     return code_challenge
